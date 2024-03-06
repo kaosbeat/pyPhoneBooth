83ee97478cf620d7aebc02ddaae79e456f95e87e
wouter.devriese@luca-arts.be <wouter.devriese@luca-arts.be>
2024-03-06 09:12:56 +0100

    updates for rpi5

--- detect_pi.py
+++ detect_pi.py
@@ -9,9 +9,27 @@ def is_raspberrypi():
         pass
     return False
 
+def raspberrypi_version():
+    try:
+        with io.open('/proc/device-tree/compatible', 'r') as m:
+            match m:
+                case "raspberrypi,5-model-bbrcm,bcm2712":
+                    return 5
+                case "raspberrypi,4-model-bbrcm,bcm2711":
+                    return 4
+                case "raspberrypi,model-zero-wbrcm,bcm2835":
+                    return 0
+                case _:
+                    print("not sure which rpi board you've got there mate")
+                    return _
+    except Exception as e:
+        print("exception reading rpi version: {}".format(e))
+    return False
 
 if __name__ == "__main__":
     if is_raspberrypi():
-        print("this script gets executed on a pi")
+        print("this script gets executed on a pi {}".format(raspberrypi_version()))
     else:
         print("not a pi this is")
+
+    
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 python -m venv env
 source env/bin/activate
-sudo apt install python3-pyaudio
+sudo apt install python3-pyaudio python3-soundfile
 pip install -r requirements.txt
 git clone https://github.com/raspberrypi/Pi-Codec.git codec_zero
