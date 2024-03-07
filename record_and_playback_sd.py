717a409a57d1886a119d0f7aed9f064019ec9a0d
wouter Devriese <devriese.wouter@gmail.com>
2024-03-07 21:20:46 +0100

    added duration for recording and normalizing

--- record_and_playback_sd.py
+++ record_and_playback_sd.py
@@ -12,26 +12,33 @@ from detect_pi import is_raspberrypi, raspberrypi_version
 import numpy  # Make sure NumPy is loaded before it is used in the callback
 
 assert numpy  # avoid "imported but unused" message (W0611)
+from pydub import AudioSegment, effects
 
 
 class AudioRecorder:
     def __init__(self, rpi_execution: bool = False):
         self.recording = None
         self.rpi = False
+        self.samplerate = 16000
+        self.block_size = 1024
+        self.channels = 1
+        self.duration = 5
         if rpi_execution:
             print("RPI execution")
             rpi_version = raspberrypi_version()
             match rpi_version:
                 case 4:
                     from gpio_class_rpigpio import gpio_class
-                    self.gpio = gpio_class(callback_function_pressed=self.adapt_recording)
+                    self.gpio = gpio_class(callback_function=self.adapt_recording)
                 case 5:
                     from gpio_class_gpiozero import gpio_class
-                    self.gpio = gpio_class(callback_function_pressed=self.start_recording, callback_function_released=self.stop_recording)
+                    self.gpio = gpio_class(callback_function_pressed=self.start_recording,
+                                           callback_function_released=self.stop_recording)
                 case _:
                     print("check which gpio class to use for this rpi {}".format(rpi_version))
                     from gpio_class_gpiozero import gpio_class
-                    self.gpio = gpio_class(callback_function_pressed=self.start_recording, callback_function_released=self.stop_recording)
+                    self.gpio = gpio_class(callback_function_pressed=self.start_recording,
+                                           callback_function_released=self.stop_recording)
             self.rpi = True
         else:
             print("running on reg pc")
@@ -42,12 +49,11 @@ class AudioRecorder:
             listener.start()
 
         # Audio parameters
-        sd.default.samplerate = 44100
-        sd.default.blocksize = 1024
-        sd.default.channels = 2
+        sd.default.samplerate = self.samplerate
+        sd.default.blocksize = self.block_size
+        sd.default.channels = self.channels
         # sd.default.dtype = 'int24'
-        self.non_blocking = True
-        sd.default.device= 1
+        sd.default.device = 'USB Audio Device'
         self.q = queue.Queue()
 
         # recording files
@@ -56,6 +62,7 @@ class AudioRecorder:
         self.latest_recording = ""
 
     def adapt_recording(self, channel):
+        """used only for rpi4 & RPI.GPIO library"""
         time.sleep(0.1)
         if GPIO.input(channel) == GPIO.LOW:
             self.start_recording()
@@ -63,13 +70,15 @@ class AudioRecorder:
             self.stop_recording()
 
     def callback(self, indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
+        """This is called (from a separate thread) for each audio block. This puts the audio in a queue so we can
+        save it in the main thread"""
         #        print(indata)
         if status:
             print(status, file=sys.stderr)
         self.q.put(indata.copy())
 
     def alter_recording_key(self, key):
+        """function used when we use a keyboard"""
         import pynput
         if type(key) == pynput.keyboard.KeyCode:
             if key.char == 'r':
@@ -97,21 +106,25 @@ class AudioRecorder:
 
         self.frames.clear()
         try:
-            if self.non_blocking:
-                with sf.SoundFile(file_name, mode='x', samplerate=44100, channels=2, subtype="PCM_24") as file:
-                    with sd.InputStream(callback=self.callback):
-                        if self.rpi:
-                            self.gpio.switch_led(to_green=True)
-                        print('#' * 80)
-                        print('press Ctrl+C to stop the recording')
-                        print('#' * 80)
-                        while self.recording:
-                            file.write(self.q.get())
-            else:
-                duration = 5  # seconds
-                my_recording = sd.rec(int(duration * 44100), samplerate=44100, channels=2)
-                sd.wait()
-                sd.play(my_recording, 44100)
+            # we calculate the timeout time
+            timeout = time.time() + self.duration
+            with sf.SoundFile(file_name, mode='x', samplerate=self.samplerate, channels=self.channels,
+                              subtype="PCM_24") as file:
+                with sd.InputStream(callback=self.callback):
+                    # debug LEDs, this may be removed if not wanted
+                    if self.rpi:
+                        self.gpio.switch_led(to_green=True)
+                    # debug text, this may be removed if not wanted
+                    print('#' * 80)
+                    print('press Ctrl+C to stop the recording')
+                    print('#' * 80)
+                    # keep recording until the button is pushed or the timeout has happened
+                    while self.recording and time.time() <= timeout:
+                        file.write(self.q.get())
+            # normalize the audio
+            raw_sound = AudioSegment.from_file(file_name, "wav")
+            normalized_sound = effects.normalize(raw_sound)
+            normalized_sound.export(file_name, format="wav")
         except sd.PortAudioError as e:
             print("PA error {}".format(e))
             # try again, miserable portaudio library
@@ -120,17 +133,18 @@ class AudioRecorder:
         self.latest_recording = file_name
 
         # play audio after recording
-        self.play_audio()
+        # TODO KASPER, remove this if you want to hook up espeak
+        self.play_audio(self.latest_recording)
 
-    def play_audio(self):
+    def play_audio(self, audio_file):
         try:
-            data, fs = sf.read(self.latest_recording)
+            data, fs = sf.read(audio_file)
             sd.play(data, fs)
             sd.wait()
         except sd.PortAudioError as e:
             print("PA error {}".format(e))
             # try again, miserable portaudio library
-            self.play_audio()
+            self.play_audio(audio_file)
 
 
 if __name__ == "__main__":
